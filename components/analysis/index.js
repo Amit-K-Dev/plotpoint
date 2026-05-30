97ca28a1fe99ee488776e68c7a51f29706948ae5
Amit Kumar <56325341+Amit-K-Dev@users.noreply.github.com>
2026-05-30 05:38:00 +0530

    feat: add AI prediction center with imdb, rotten tomatoes and box office forecasts

--- components/analysis/index.js
+++ components/analysis/index.js
@@ -2,3 +2,4 @@ export { EmotionArc }    from "./EmotionArc";
 export { KeyMoments }    from "./KeyMoments";
 export { TheoryCard }    from "./TheoryCard";
 export { HypeBreakdown } from "./HypeBreakdown";
+export * from "./PredictionCenter";
