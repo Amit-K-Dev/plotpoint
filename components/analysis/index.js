7607cbd834adde84421c8dfd3ff299d093dc4546
Amit Kumar <56325341+Amit-K-Dev@users.noreply.github.com>
2026-05-31 11:22:17 +0530

    improves index

--- components/analysis/index.js
+++ components/analysis/index.js
@@ -1,8 +1,9 @@
-export { EmotionArc }    from "./EmotionArc";
-export { KeyMoments }    from "./KeyMoments";
-export { TheoryCard }    from "./TheoryCard";
+export { EmotionArc } from "./EmotionArc";
+export { KeyMoments } from "./KeyMoments";
+export { TheoryCard } from "./TheoryCard";
 export { HypeBreakdown } from "./HypeBreakdown";
-export * from "./PredictionCenter";
-export * from "./StrengthWeakness";
-export * from "./HiddenClues";
+
+export { PredictionCenter } from "./PredictionCenter";
+export { StrengthWeakness } from "./StrengthWeakness";
+export { HiddenClues } from "./HiddenClues";
 export * from "./CharacterPredictions";
