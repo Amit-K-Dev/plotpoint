ffb7b1cdaa7657958f3e8e648d4a9f46d553ec03
Amit Kumar <56325341+Amit-K-Dev@users.noreply.github.com>
2026-05-31 12:47:49 +0530

    improves index.js

--- components/analysis/index.js
+++ components/analysis/index.js
@@ -6,4 +6,4 @@ export { HypeBreakdown } from "./HypeBreakdown";
 export { PredictionCenter } from "./PredictionCenter";
 export { StrengthWeakness } from "./StrengthWeakness";
 export { HiddenClues } from "./HiddenClues";
-export * from "./CharacterPredictions";
+export { CharacterPredictions } from "./CharacterPredictions";
